1e3ae406758ba4354193d707bbf06328f23fd516
farilucas <lucas450045@gmail.com>
2019-06-15 18:49:57 -0300

    Se sube la mejora del Sistema, está comentado pq el makefile tira algunos errores

--- LabPafinal/Clases/Headers/Sistema.h
+++ LabPafinal/Clases/Headers/Sistema.h
@@ -15,7 +15,20 @@ class Sistema  {
     IDictionary* cines;
  public:
    static Sistema* getInstance();
-   void ListarTitulos();
    void RegistrarUsuario(string, string, string);
-
+   ~Sistema();
+   void agregarUsuario(string nickname, string fotoperfil, string Pass);
+   void IniciarSesion(string Nickname, string Pass);
+   void ListarTitulos();
+   void Eliminar(string Titulo);
+   void ListarPeliculas();
+   void SeleccionPelicula(string Titulo);
+   void ListarCines();
+   void SeleccionCine(int NroCine);
+   void ListarFunciones(int NroFuncion);
+   float PagaCredito(string NombreFinanciera, int CantAsientos);
+   float PagaDebito(string NombreBanco, int CantAsientos);
+   void CrearReserva();
+   void Eliminar2(string Titulo);
+   void SeleccionTitulo(string Titulo);
 };
--- LabPafinal/Clases/Source/Sistema.cpp
+++ LabPafinal/Clases/Source/Sistema.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "../Headers/Sistema.h"
+#include "../../String.h"
 
 using namespace std;
 
@@ -15,3 +16,89 @@ using namespace std;
     throw invalid_argument("El usuario ya existe");
   }
 }*/
+
+/*void Sistema::IniciarSesion(string Nickname, string Pass){
+  String* key = new String(Nickname);
+  Usuarios* U = new Usuarios(Nickname, Pass);
+  Usuarios->add(U,key);
+}*/
+
+// void Sistema::agregarUsuario(string nickname, string fotoperfil, string Pass){
+//   String* Key = new String(nickname);
+//   if(!Usuario->member(Key)){
+//     Usuarios->add(Key,new Usuario(nickname,fotoperfil,Pass));
+//   }
+//   else{
+//     delete Key;
+//     throw invalid_argument("El usuario ya existe");
+//   }
+// }
+
+
+// void Sistema::ListarTitulos(Pelicula::Titulo){
+//   IKey* key = new KeyInteger(Titulo);
+//   ICollectible* p = Peliculas->find(key);
+//   delete key;
+//   if(p==NULL){
+//     throw invalid_argument("No hay Peliculas");
+//   }
+//   Peliculas->add(key);
+// }
+//
+// void Sistema::Eliminar(Pelicula::Titulo){
+//   IKey* key = new KeyInteger(Titulo);
+//   ICollectible* p = Peliculas->find(key);
+//   delete key;
+//   if(p==NULL){
+//     throw invalid_argument("No hay Peliculas");
+//   }
+//   Peliculas->remove(key);
+//   delete p;
+// }
+
+/*void Sistema::ListarPeliculas(DtPelicula::DtPelicula){
+  //Dudas
+}*/
+
+// void Sistema::SeleccionPelicula(Pelicula::Titulo){
+//
+// }
+
+// void Sistema::ListarCines(DtCine::DtCine){
+//
+// }
+
+// void Sistema::SeleccionCine(Cine::NroCine){
+//
+// }
+
+// void Sistema::ListarFunciones(Funcion::NroFuncion){
+//
+// }
+
+// float Sistema::PagaCredito(string NombreFinanciera, int CantAsientos){
+//
+// }
+//
+// float Sistema::PagaDebito(string NombreBanco, int CantAsientos){
+//
+// }
+//
+// void Sistema:CrearReserva(){
+//
+// }
+//
+// void Sistema::Eliminar2(string Titulo){
+//   IKey* key = new KeyInteger(Titulo);
+//   ICollectible* /*F*/ = /*Funciones*/->find(key);
+//   delete key;
+//   if(p==NULL){
+//     throw invalid_argument("No hay Peliculas");
+//   }
+//   /*Funciones*/->remove(key);
+//   delete /*F*/;
+// }
+//
+// void Sistema::SeleccionTitulo(string Titulo){
+//
+// }
